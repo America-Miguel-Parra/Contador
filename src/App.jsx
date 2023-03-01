518c7ace431b2244f28f850f3392cb8bbed8ed83
America-Miguel-Parra <isc19350307@gmail.com>
2023-03-01 10:08:51 -0600

    Contador que incrementa numeros

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function App() {
+  //let Counter = 0;
+  const [Counter, setCounter] = useState(0)
+
+  return (
+   <div>
+    <h3>Counter</h3>
+    <hr/>
+    <h1>{Counter}</h1>
+    <button
+    onClick={()=> {
+      setCounter(Counter +1);
+      console.log(Counter)
+    }} 
+    type="button" 
+    className="btn btn-primary"
+    >+1</button>
+  </div> 
+  )
+
+  
+}
+
+export default App
